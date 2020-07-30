5d838992dc4ad9b6a62faff9bfa369c3e63aa112
Baxter Pearson <baxterrpearson@gmail.com>
2020-07-30 17:17:40 +0100

    Admin nav started

---
+++ Areas/ClientAdmin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairDemoSite.Areas.ClientAdmin.Controllers
+{
+    [Area("ClientAdmin")]
+    [Authorize(Roles = "Admin")]
+    //[AllowAnonymous]
+    public class UserController : Controller
+    {
+        [Route("ClientAdmin/CreateUser")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
